6cc2fc2d64c1f137d44bcbd3010bfb393d80eb9d
Richard Körösi <korosi.richard@gmail.com>
2024-02-13 21:02:33 +0100

    Write Error

--- zadanie01.py
+++ zadanie01.py
@@ -1,22 +1,26 @@
 import sys
 
 f = open(sys.argv[1], "r")
+currentRow = 1
 
 
-def read(inp):
+def read(inp, currentRow):
     print(f"Zadajte hodnotu premennej {inp}:", end="")
     globals()[inp] = input()
 
 
-def write(inp):
-    if globals()[inp] == "None":
-        print(f"Obsah premennej {inp}:")
+def write(inp, row):
+    if inp not in globals():
+        error(row, "non_existent_variable", inp)
+        exit()
     else:
         print(f"Obsah premennej {inp}: {globals()[inp]}")
 
 
-def error(row):
-    print(f"CHYBA NA RIADKU {str(row)}!!!")
+def error(row, errorType, variable=None):
+    errorDictionary = errorsDictionary(variable)
+    print(f"CHYBA NA RIADKU {str(row)}!!!", end=" ")
+    print(errorDictionary[errorType])
 
 
 def plus(i, j, k):
@@ -43,10 +47,11 @@ for line in f:
     arguments = line[1:]
     if function in interpreterDictionary:
         if len(arguments) == 1:
-            interpreterDictionary[function](arguments[0])
+            interpreterDictionary[function](arguments[0], currentRow)
         elif len(arguments) == 2:
-            interpreterDictionary[function](arguments[0], arguments[1])
+            interpreterDictionary[function](arguments[0], arguments[1], currentRow)
         elif len(arguments) == 3:
-            interpreterDictionary[function](arguments[0], arguments[1], arguments[2])
+            interpreterDictionary[function](arguments[0], arguments[1], arguments[2], currentRow)
         else:
             interpreterDictionary[function]()
+    currentRow += 1
